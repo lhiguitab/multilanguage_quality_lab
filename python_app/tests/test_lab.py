8678a1f9f07f63cba9ffde39f60fd5b779a0e3be
lhiguitab <lhiguitab@eafit.edu.co>
2026-03-04 10:10:32 -0500

    fix: eliminación de import no utilizado

--- python_app/tests/test_lab.py
+++ python_app/tests/test_lab.py
@@ -4,8 +4,6 @@
 # - "radar" -> True, "anita lava la tina" -> True, "python" -> False, "" -> True, "Radar" -> True
 # - suma(2,3) -> 5; suma(0,5) -> 5; suma(-2,3) -> 1
 
-from utils import suma
-
 
 def test_ejemplo_siembra():
     assert True
